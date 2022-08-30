0a1901daf5d452c89cab38c2366f27e3f8bbce4d
Joey <joey.kheireddine@outlook.com>
2022-08-30 17:28:53 +0200

    upd: created blockswidget for frontpage

--- pages/component/blocksWidget.js
+++ pages/component/blocksWidget.js
@@ -11,9 +11,34 @@ export default function BlocksWidget() {
           <button>View All</button>
         </div>
       </div>
-      <div className="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
-        <div>Hello World!</div>
+      <div className="border-b border-gray-200 bg-white px-4 py-5 sm:px-6 space-y-3">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, key) => (
+          <BlockItem
+            key={key}
+            height={5000}
+            extrinsics={25}
+            events={30}
+            timestamp={34000}
+          />
+        ))}
       </div>
     </div>
   );
 }
+
+const BlockItem = ({ height, extrinsics, events, timestamp }) => {
+  return (
+    <div className="border-b-gray-100 block">
+      <div className="flex flex-row justify-between">
+        <div className="font-bold">Block# {height}</div>
+      </div>
+      <div className="flex flex-row justify-between">
+        <div>
+          <span className="text-gray-500">Includes</span> {extrinsics}
+          <span className="text-gray-500">Events</span> {events}
+        </div>
+        <div>{timestamp}</div>
+      </div>
+    </div>
+  );
+};
