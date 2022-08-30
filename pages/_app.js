82adb0155074271f94ab329a77dfbdd1a8db0a27
Joey <joey.kheireddine@outlook.com>
2022-08-30 19:25:44 +0200

    upd: created a simple footer

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,17 @@ import "../styles/globals.css";
 import Header from "../components/header";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Search from "../components/search";
+import Footer from "../components/footer";
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="h-screen bg-[#fbfbfd]">
+      <div className="h-full min-h-screen bg-[#fbfbfd]">
         <Header />
         <Search />
         <Component {...pageProps} />
+        <Footer/>
       </div>
     </QueryClientProvider>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -8,4 +10,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
