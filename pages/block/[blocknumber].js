240a36fbaf2d336e2bf9b173a8180171007296c4
Joey <joey.kheireddine@outlook.com>
2022-08-30 23:39:44 +0200

    upd: added all details for block, and be able to go back a block

--- pages/block/[blocknumber].js
+++ pages/block/[blocknumber].js
@@ -5,7 +5,9 @@ import { CubeIcon } from "@heroicons/react/24/outline";
 import { useQuery } from "@tanstack/react-query";
 import LoadingBlock from "../../components/loadingBlock";
 import * as clientApi from "../../utils/client";
+import moment from "moment";
 import { sleep } from "../../utils/utils";
+import Link from 'next/link'
 
 export default function Block() {
   const router = useRouter();
@@ -22,6 +24,14 @@ export default function Block() {
     }
   );
 
+  const getPrevBlock = () => {
+    if (parseInt(blocknumber) == 0) {
+      return 0;
+    } else {
+      return parseInt(blocknumber) - 1;
+    }
+  };
+
   return (
     <ContainerLayout>
       <PageHeader
@@ -31,71 +41,81 @@ export default function Block() {
       {query.isLoading ? (
         <LoadingBlock title={`Block ${blocknumber}`} />
       ) : (
-        <div className="overflow-hidden bg-white border border-gray-100 rounded-md shadow-md mt-5">
-          <div className=" px-4 py-5 sm:p-0">
-            <dl className="sm:divide-y sm:divide-gray-200">
-              <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-                <dt className="text-sm font-medium text-gray-900">Timestamp</dt>
-                <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
-                  {query.data.timestamp}
-                </dd>
-              </div>
+        <>
+          <div className="overflow-hidden bg-white border border-gray-100 rounded-md shadow-md mt-5">
+            <div className=" px-4 py-5 sm:p-0">
+              <dl className="sm:divide-y sm:divide-gray-200">
+                <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-900">
+                    Timestamp
+                  </dt>
+                  <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
+                    {moment(query.data.timestamp).format("LLL")}
+                  </dd>
+                </div>
 
-              <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-                <dt className="text-sm font-medium text-gray-900">Status</dt>
-                <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
-                  {query.data.status ? "Finalized" : "Unfinalized"}
-                </dd>
-              </div>
+                <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-900">Status</dt>
+                  <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
+                    {query.data.status ? "Finalized" : "Unfinalized"}
+                  </dd>
+                </div>
 
-              <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-                <dt className="text-sm font-medium text-gray-900">Hash</dt>
-                <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
-                  {query.data.hash}
-                </dd>
-              </div>
+                <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-900">Hash</dt>
+                  <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
+                    {query.data.hash}
+                  </dd>
+                </div>
 
-              <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-                <dt className="text-sm font-medium text-gray-900">
-                  Parent Hash
-                </dt>
-                <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
-                  {query.data.parentHash}
-                </dd>
-              </div>
-              <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-                <dt className="text-sm font-medium text-gray-900">
-                  State Root
-                </dt>
-                <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
-                  {query.data.stateRoot}
-                </dd>
-              </div>
-              <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-                <dt className="text-sm font-medium text-gray-900">
-                  Extrinsics Root
-                </dt>
-                <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
-                  {query.data.extrinsicsRoot}
-                </dd>
-              </div>
-              <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-                <dt className="text-sm font-medium text-gray-900">Collator</dt>
-                <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
-                  {query.data.collator}
-                </dd>
-              </div>
-              <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-                <dt className="text-sm font-medium text-gray-900">
-                  Spec Version
-                </dt>
-                <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
-                  {query.data.specVersion}
-                </dd>
-              </div>
-            </dl>
+                <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-900">
+                    Parent Hash
+                  </dt>
+                  <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
+                    <Link href={`/block/${getPrevBlock()}`}>
+                      <span className="cursor-pointer text-indigo-500">
+                        {query.data.parentHash}
+                      </span>
+                    </Link>
+                  </dd>
+                </div>
+                <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-900">
+                    State Root
+                  </dt>
+                  <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
+                    {query.data.stateRoot}
+                  </dd>
+                </div>
+                <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-900">
+                    Extrinsics Root
+                  </dt>
+                  <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
+                    {query.data.extrinsicsRoot}
+                  </dd>
+                </div>
+                <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-900">
+                    Collator
+                  </dt>
+                  <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
+                    {query.data.collator}
+                  </dd>
+                </div>
+                <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-900">
+                    Spec Version
+                  </dt>
+                  <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">
+                    {query.data.specVersion}
+                  </dd>
+                </div>
+              </dl>
+            </div>
           </div>
-        </div>
+        </>
       )}
     </ContainerLayout>
   );
