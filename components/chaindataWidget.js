062219f831f26f026f6bc20077913d5eb9c4b3e1
Joey <joey.kheireddine@outlook.com>
2022-08-31 16:52:59 +0200

    upd: added icons to statistics

--- components/chaindataWidget.js
+++ components/chaindataWidget.js
@@ -1,4 +1,8 @@
-import { ChartPieIcon } from "@heroicons/react/24/outline";
+import {
+  ChartPieIcon,
+  UserGroupIcon,
+  CubeIcon,
+} from "@heroicons/react/24/outline";
 import { sleep } from "../utils/utils";
 import { useQuery } from "@tanstack/react-query";
 import * as clientApi from "../utils/client";
@@ -40,15 +44,31 @@ export default function ChaindataWidget() {
         <div className="space-y-3">
           <dl className="mt-5 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-y-0 md:divide-x">
             {query.data.map((item) => (
-              <div key={item.name} className="px-4 py-5 sm:p-6">
-                <dt className="text-base text-gray-900 font-semibold">
-                  {item.name}
-                </dt>
-                <dd className="mt-1 flex items-baseline justify-between md:block lg:flex">
-                  <div className="flex items-baseline text-2xl font-semibold text-indigo-600">
-                    <CountUp end={item.stat} separator={","} />
-                  </div>
-                </dd>
+              <div
+                key={item.name}
+                className="px-4 py-5 sm:p-6 flex flex-row my-auto"
+              >
+                <div className="my-auto pr-3">
+                  {item.name === "Transfers" && (
+                    <ChartPieIcon className="h-12 text-gray-800" />
+                  )}
+                  {item.name === "Finalized Blocks" && (
+                    <CubeIcon className="h-12 text-gray-800" />
+                  )}
+                  {item.name === "Holders" && (
+                    <UserGroupIcon className="h-12 text-gray-800" />
+                  )}
+                </div>
+                <div>
+                  <dt className="text-base text-gray-900 font-semibold">
+                    {item.name}
+                  </dt>
+                  <dd className="mt-1 flex items-baseline justify-between md:block lg:flex">
+                    <div className="flex items-baseline text-2xl font-semibold text-indigo-600">
+                      <CountUp end={item.stat} separator={","} />
+                    </div>
+                  </dd>
+                </div>
               </div>
             ))}
           </dl>
