9dc4e99e07d61527f75ac2d6ce90f1a6aaf6e356
aidan-starke <starke.aidan@gmail.com>
2022-09-09 08:15:57 +1200

    Prettier format

--- components/search.js
+++ components/search.js
@@ -32,7 +32,7 @@ export default function Search() {
 			setError("Invalid search parameter");
 		}
 	};
-	
+
 	return (
 		<div className="border-b border-b-gray-200 bg-white">
 			<div className="mx-auto max-w-7xl py-3 px-4 sm:px-6 lg:px-8">
