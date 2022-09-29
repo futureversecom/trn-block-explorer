fdf3eab57b15f62562a74fbb9731ff702f3a4980
James Carolan <james.carolan87@gmail.com>
2022-09-29 15:45:22 +1300

    Add <Head/> comp and favicons.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-
+import Head from "next/head"
 import { Footer, Header, Search } from "@/components";
 import { queryClient } from "@/libs/client";
 import "@/styles/globals.css";
@@ -11,7 +11,13 @@ function MyApp({ Component, pageProps }) {
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<div className="h-full min-h-screen bg-[#111]">
+				<div className="h-full min-h-screen bg-[#111]">
+				<Head>
+					<title>The Root Network Block Explorer</title>
+					<meta name="description" content="Explore and search blocks, extrinsics and accounts on The Root Network."/>
+					<link rel="apple-touch-icon" sizes="256x256" href="/favicon_big"/>
+					<link rel="icon" type="image/png" sizes="32x32" href="/favicon.png"/>
+				</Head>
 				<Header />
 				<Search />
 				<Component {...pageProps} />
