b9991859bca3882dc16acbf88270b3b77177c09b
Joey <joey.kheireddine@outlook.com>
2022-10-12 23:09:10 +0200

    upd: added self colors to inoutlabel

--- components/inOutLabel.js
+++ components/inOutLabel.js
@@ -1,12 +1,17 @@
 const InOutLabel = ({ type }) => {
 	const incoming = "bg-green-300 text-green-800";
 	const outgoing = "bg-orange-300 text-orange-800";
+	const self = "bg-blue-300 text-blue-800";
 	const theType = type.toLowerCase();
+
+	const classes = {
+		out: outgoing,
+		in: incoming,
+		self: self,
+	};
 	return (
 		<div
-			className={`w-8 p-1 text-center font-bold ${
-				theType === "out" ? outgoing : incoming
-			} capitalize`}
+			className={`w-8 p-1 text-center font-bold ${classes[theType]} capitalize`}
 		>
 			{type}
 		</div>
