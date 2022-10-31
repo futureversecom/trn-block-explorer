e1b6be2f5d2567e3f31ab7ae7b7635aefbe4672d
Ken Vu <ken.vu@centrality.ai>
2022-10-31 14:50:20 +1300

    Remove redundant wallet address in account page

--- components/pageHeader.js
+++ components/pageHeader.js
@@ -1,19 +1,23 @@
 import Head from "next/head";
 import { Fragment } from "react";
 
-export default function PageHeader({ title, icon }) {
+export default function PageHeader({ title, icon, children }) {
 	return (
 		<Fragment>
 			<Head>
 				<title>Root Explorer - {title}</title>
-				<meta name="description" content="Root Network Explorer by Futureverse" />
+				<meta
+					name="description"
+					content="Root Network Explorer by Futureverse"
+				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<div className="flex py-3">
+			<div className="flex items-center py-3">
 				{icon}
 				<h3 className="text-md truncate font-bold leading-6 text-white">
 					{title}
 				</h3>
+				{children}
 			</div>
 		</Fragment>
 	);
