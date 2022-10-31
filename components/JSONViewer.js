37bcbc646d13e6db1b3076c7e8074cbb7ffb5e62
aidan-starke <starke.aidan@gmail.com>
2022-10-31 16:34:14 +1300

    Set `JSONViewer` bg to transparent

--- components/JSONViewer.js
+++ components/JSONViewer.js
@@ -2,5 +2,11 @@ import JSONPretty from "react-json-pretty";
 import JSONPrettyMonokai from "react-json-pretty/dist/monikai";
 
 export default function JSONViewer({ data }) {
-	return <JSONPretty data={data} theme={JSONPrettyMonokai} />;
+	return (
+		<JSONPretty
+			data={data}
+			theme={JSONPrettyMonokai}
+			themeClassName="!bg-transparent"
+		/>
+	);
 }
