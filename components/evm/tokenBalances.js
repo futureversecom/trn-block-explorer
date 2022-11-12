bbce5d77683fb9aa76356b946a94571a2e9e1c7d
Joey <joey.kheireddine@outlook.com>
2022-11-12 23:28:21 +0100

    upd: more work on TokenBalances component

--- components/evm/tokenBalances.js
+++ components/evm/tokenBalances.js
@@ -6,11 +6,11 @@ function classNames(...classes) {
 	return classes.filter(Boolean).join(" ");
 }
 
-export default function TokenBalances() {
+export default function TokenBalances({ walletAddress }) {
 	return (
 		<Menu as="div" className="relative inline-block text-left">
 			<div>
-				<Menu.Button className="inline-flex w-full justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+				<Menu.Button className="inline-flex w-full justify-between border border-gray-300 bg-transparent px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
 					Retrieving tokens..
 					<ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
 				</Menu.Button>
@@ -25,9 +25,9 @@ export default function TokenBalances() {
 				leaveFrom="transform opacity-100 scale-100"
 				leaveTo="transform opacity-0 scale-95"
 			>
-				<Menu.Items className="absolute right-0 z-10 mt-2 w-full origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+				<Menu.Items className="absolute right-0 z-10 mt-2 w-full origin-top-right divide-y divide-gray-100 bg-[#111] text-white border border-gray-300 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
 					<div className="px-4 py-3">
-						<p className="text-sm">ERC20 Tokens</p>
+						<p className="font-semibold">ERC20 Tokens</p>
 					</div>
 					<div className="py-1">
 						<Menu.Item>
@@ -35,11 +35,41 @@ export default function TokenBalances() {
 								<a
 									href="#"
 									className={classNames(
-										active ? "bg-gray-900 text-gray-100" : "text-gray-900",
-										"block px-4 py-2 text-sm bg-gray-900"
+										active ? "text-gray-200" : "text-white",
+										"block px-4 py-2 text-sm"
 									)}
 								>
-									Account settings
+									<div className="flex justify-between">
+										<div className="flex flex-col">
+											<span>Sylo (SYLO)</span>
+											<span>0x00000000000000000000</span>
+										</div>
+										<div className="my-auto">100000</div>
+									</div>
+								</a>
+							)}
+						</Menu.Item>
+					</div>
+					<div className="px-4 py-3">
+						<p className="font-semibold">ERC721 Tokens</p>
+					</div>
+					<div className="py-1">
+						<Menu.Item>
+							{({ active }) => (
+								<a
+									href="#"
+									className={classNames(
+										active ? "text-gray-200" : "text-white",
+										"block px-4 py-2 text-sm"
+									)}
+								>
+									<div className="flex justify-between">
+										<div className="flex flex-col">
+											<span>Scenes & Sounds</span>
+											<span>0x00000000000000000000</span>
+										</div>
+										<div className="my-auto">25 NFT(s)</div>
+									</div>
 								</a>
 							)}
 						</Menu.Item>
