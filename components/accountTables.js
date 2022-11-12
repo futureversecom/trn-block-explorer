f10fa2481284084f10fcf4226b27a166f68fbc1a
Joey <joey.kheireddine@outlook.com>
2022-11-12 23:09:33 +0100

    upd: realigned tablist

--- components/accountTables.js
+++ components/accountTables.js
@@ -7,21 +7,26 @@ import { Fragment } from "react";
 import { useAccountRefetchStatus } from "@/libs/stores";
 
 import {
+	Erc20TransfersForAddress,
+	Erc721TransfersForAddress,
 	EvmTransactionsForAddress,
 	RefetchIndicator,
 	TransfersForAddress,
-	Erc721TransfersForAddress,
-	Erc20TransfersForAddress
 } from "./";
 
 export const AccountTables = ({ walletAddress }) => {
 	const isRefetching = useAccountRefetchStatus();
 	const router = useRouter();
 	// tab name -> index <-- tab name
-	const url_to_index = ["transfers", "evm_transactions", "erc721_transfers", "erc20_transfers"];
+	const url_to_index = [
+		"transfers",
+		"evm_transactions",
+		"erc721_transfers",
+		"erc20_transfers",
+	];
 
 	return (
-		<div className="mt-6">
+		<div>
 			<Tab.Group
 				defaultIndex={url_to_index.indexOf(router?.query?.tab) || 0}
 				onChange={(e) => {
@@ -34,7 +39,14 @@ export const AccountTables = ({ walletAddress }) => {
 					});
 				}}
 			>
-				<TabList titles={["Transfers", "EVM Transactions", "ERC721 Transfers", "ERC20 Transfers"]}>
+				<TabList
+					titles={[
+						"Transfers",
+						"EVM Transactions",
+						"ERC721 Transfers",
+						"ERC20 Transfers",
+					]}
+				>
 					{isRefetching && (
 						<div className="absolute right-2 top-6 flex">
 							<RefetchIndicator />
@@ -55,14 +67,14 @@ export const AccountTables = ({ walletAddress }) => {
 };
 
 const TabList = ({ titles, children }) => (
-	<Tab.List className="relative mb-2 flex">
+	<Tab.List className="relative flex space-x-2 mb-3">
 		{titles.map((title, key) => (
 			<Fragment key={key}>
 				<Tab className="text-white outline-none">
 					{({ selected }) => (
 						<span
 							className={clsx(
-								"inline-block bg-transparent p-4",
+								"inline-block bg-transparent px-2 py-1",
 								selected && "border-b-2 border-gray-500 text-white duration-300"
 							)}
 						>
