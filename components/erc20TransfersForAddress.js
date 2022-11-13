9af9462e8a4f53b16bb39105d73db8a21bd40143
Joey <joey.kheireddine@outlook.com>
2022-11-13 01:07:28 +0100

    upd: added amount of transfers above erc20 table

--- components/erc20TransfersForAddress.js
+++ components/erc20TransfersForAddress.js
@@ -28,6 +28,14 @@ export default function Erc20TransfersForAddress({ walletAddress }) {
 				<LoadingBlock title="ERC20 Transfers" height="h-20" />
 			) : (
 				<div className="divide-y overflow-x-auto border border-gray-400 text-white">
+					{query?.data?.docs?.length ? (
+						<div className="text-sm px-3 py-3">
+							Displaying {parseInt(query?.data?.limit * query?.data?.page)} from
+							a total of {query?.data?.totalDocs} transfers
+						</div>
+					) : (
+						<Fragment />
+					)}
 					{query?.data?.docs?.length > 0 ? (
 						<TableLayout.Table>
 							<thead className="bg-transparent">
