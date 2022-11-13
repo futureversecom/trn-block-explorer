01841fb9014f08ca5c8e1959a0cd20aa71effbdc
Joey <joey.kheireddine@outlook.com>
2022-11-13 01:57:38 +0100

    upd: use new tooltip across the board

--- components/evm/evmTooltip.js
+++ components/evm/evmTooltip.js
@@ -2,11 +2,11 @@ import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
 import { Fragment } from "react";
 import ReactTooltip from "react-tooltip";
 
-export default function EVMTooltip({ message }) {
+export default function EVMTooltip({ message, children }) {
 	return (
 		<Fragment>
 			<p data-tip={message}>
-				<QuestionMarkCircleIcon className="h-5 w-5" />
+				{children ? children : <QuestionMarkCircleIcon className="h-5 w-5" />}
 			</p>
 			<ReactTooltip />
 		</Fragment>
