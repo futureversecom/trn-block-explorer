f12bb019cb2807ed3e14169da4b04d53c1e903e1
Joey <joey.kheireddine@outlook.com>
2022-11-13 17:24:59 +0100

    upd: added tooltip to EVM status of transaction

--- components/evm/TransactionStatus.js
+++ components/evm/TransactionStatus.js
@@ -5,6 +5,8 @@ import {
 } from "@heroicons/react/24/outline";
 import { Fragment } from "react";
 
+import EVMTooltip from "@/components/evm/evmTooltip";
+
 export default function TransactionStatus({ tx }) {
 	let status;
 
@@ -29,22 +31,28 @@ export default function TransactionStatus({ tx }) {
 	return (
 		<Fragment>
 			{status == 0 && (
-				<span className="inline-flex items-center rounded bg-gray-100 px-1 space-x-1 py-0.5 text-xs font-medium text-gray-800">
-					<ClockIcon className={iconClass} />
-					<div>Pending</div>
-				</span>
+				<EVMTooltip message="This transaction is waiting to be mined.">
+					<span className="select-none inline-flex items-center rounded bg-gray-100 px-1 space-x-1 py-0.5 text-xs font-medium text-gray-800">
+						<ClockIcon className={iconClass} />
+						<div>Pending</div>
+					</span>
+				</EVMTooltip>
 			)}
 			{status == 1 && (
-				<span className="inline-flex items-center rounded bg-green-100 px-1 space-x-1  py-0.5 text-xs font-medium text-green-800">
-					<CheckIcon className={iconClass} />
-					<div>Success</div>
-				</span>
+				<EVMTooltip message="This transaction was successfully mined in a block.">
+					<span className="select-none inline-flex items-center rounded bg-green-100 px-1 space-x-1  py-0.5 text-xs font-medium text-green-800">
+						<CheckIcon className={iconClass} />
+						<div>Success</div>
+					</span>
+				</EVMTooltip>
 			)}
 			{status == 2 && (
-				<span className="inline-flex items-center rounded bg-red-100 px-1 space-x-1 py-0.5 text-xs font-medium text-red-800">
-					<ExclamationCircleIcon className={iconClass} />
-					<div>Reverted</div>
-				</span>
+				<EVMTooltip message="This transaction was reverted.">
+					<span className="select-none inline-flex items-center rounded bg-red-100 px-1 space-x-1 py-0.5 text-xs font-medium text-red-800">
+						<ExclamationCircleIcon className={iconClass} />
+						<div>Reverted</div>
+					</span>
+				</EVMTooltip>
 			)}
 		</Fragment>
 	);
