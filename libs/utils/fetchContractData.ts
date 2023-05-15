1359440c5c8d97fa87dab428fedb1e822855cd43
aidan-starke <starke.aidan@gmail.com>
2023-05-15 16:56:31 +1200

    Fetch & display contract data from sourcify

---
+++ libs/utils/fetchContractData.ts
@@ -0,0 +1,33 @@
+import { pipe } from "fp-ts/lib/function";
+import * as TE from "fp-ts/TaskEither";
+
+import { ROOT_NETWORK } from "@/libs/constants";
+import { isContract } from "@/libs/evm-api";
+
+export async function fetchContractData(address: string) {
+	return await pipe(
+		TE.Do,
+		TE.bind("contractData", () =>
+			TE.tryCatch(
+				() => isContract(address),
+				() => console.warn(`Error fetching bytecode for address ${address}`)
+			)
+		),
+		TE.bind("files", () =>
+			TE.tryCatch(
+				() =>
+					fetch(
+						`https://sourcify.dev/server/files/${ROOT_NETWORK.ChainId}/${address}`
+					).then((res) => res.json()),
+				() => console.warn(`Error fetching files for address ${address}`)
+			)
+		),
+		TE.match(
+			(_err) => null,
+			({ files, contractData }) => ({
+				files,
+				...contractData,
+			})
+		)
+	)();
+}
