1f599229af6c5b47191020b3b3d4f6743421acd7
Ken Vu <ken.vu@futureverse.com>
2023-11-27 22:11:36 +1300

    Add ROOT and VORTEX token icons

--- components/icons/tokenIcon.js
+++ components/icons/tokenIcon.js
@@ -2,8 +2,10 @@ import Image from "next/future/image";
 
 import ASTOIcon from "@/components/icons/currencies/asto.png";
 import EthIcon from "@/components/icons/currencies/eth.png";
+import RootIcon from "@/components/icons/currencies/root.png";
 import SyloIcon from "@/components/icons/currencies/sylo.png";
 import USDCIcon from "@/components/icons/currencies/usdc.png";
+import VortexIcon from "@/components/icons/currencies/vortex.png";
 import XRPIcon from "@/components/icons/currencies/xrp.png";
 
 const Icons = {
@@ -12,6 +14,8 @@ const Icons = {
 	ETH: EthIcon,
 	XRP: XRPIcon,
 	USDC: USDCIcon,
+	ROOT: RootIcon,
+	VTX: VortexIcon,
 };
 
 export default function TokenIcon({ symbol, height, width, iconClassName }) {
